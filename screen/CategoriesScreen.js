e68971cd547179fbba0de2358abbd0c04c224504
henselldelarosa <hensell1234@gmail.com>
2024-07-06 18:52:07 -0400

    style categories

---
+++ screen/CategoriesScreen.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { View, FlatList, StyleSheet } from 'react-native'
+import {CATEGORIES} from '../data/dummy-data'
+import CategoryGridTile from '../components/CategoryGridTile'
+
+
+const renderCategoryItem =(itemData) =>{
+  return <CategoryGridTile title={itemData.item.title} color={itemData.item.color}/>
+}
+
+const CategoriesScreen = () => {
+  return (
+    <FlatList
+    data={CATEGORIES}
+    keyExtractor={(itemData) => itemData.id}
+    renderItem={renderCategoryItem}
+    numColumns={2}
+    />
+  // <View></View>
+  )
+}
+
+export default CategoriesScreen
+
+const styles = StyleSheet.create({
+  
+})
