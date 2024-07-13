d003bcc3b304f0156111f69694be945c5ca0712d
Henselldelarosa <hensell1234@gmail.com>
2024-07-12 22:48:35 -0400

    added drawer navigator

--- App.js
+++ App.js
@@ -3,11 +3,49 @@ import {  StyleSheet } from 'react-native';
 import CategoriesScreen from './screen/CategoriesScreen';
 import {NavigationContainer} from '@react-navigation/native'
 import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import MealsOverViewScreen from './screen/MealsOverViewScreen';
 import MealDetailScreen from './screen/MealDetailScreen';
+import FavoriteScreen from './screen/FavoriteScreen';
+import {Ionicons} from '@expo/vector-icons'
 
 
 const Stack = createNativeStackNavigator()
+const Drawer = createDrawerNavigator()
+
+const DraweNavigator = () => {
+
+  return(
+    <Drawer.Navigator
+    screenOptions={{
+      headerStyle: {backgroundColor: '#351401'},
+      headerTintColor:'white',
+      sceneContainerStyle: {backgroundColor: '#3f2f25'},
+      drawerContentStyle: {backgroundColor: '#351401'},
+      drawerInactiveTintColor: 'white',
+      drawerActiveTintColor: '#e4baa1'
+    }}>
+      <Drawer.Screen
+      name='Categories'
+      component={CategoriesScreen}
+      options={{
+        drawerIcon : ({color, size}) => (
+          <Ionicons name='list' color={color} size={size}/>
+        )
+      }}
+      />
+      <Drawer.Screen
+      name='Favorite'
+      component={FavoriteScreen}
+      options={{
+        drawerIcon : ({color, size}) => (
+          <Ionicons name='star' color={color} size={size}/>
+        )
+      }}
+      />
+    </Drawer.Navigator>
+  )
+}
 
 export default function App() {
   return (
@@ -23,9 +61,13 @@ export default function App() {
         }}
       >
         <Stack.Screen
-        name=' Catergories'
-        component={CategoriesScreen}
+        name='Drawer'
+        component={DraweNavigator}
+        options={{
+          headerShown: false
+        }}
         />
+
         <Stack.Screen
         name='Meals'
         component={MealsOverViewScreen}
